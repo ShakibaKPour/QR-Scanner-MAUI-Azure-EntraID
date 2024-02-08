4918596196be4611a58c1569dce81b7cda71fb03
Shakiba Kouchakpour <shakiba.kouchakpour@cgi.com>
2024-02-08 15:50:12 +0100

    Implemeted translation for write report

--- ViewModels/WriteReportViewModel.cs
+++ ViewModels/WriteReportViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using RepRepair.Extensions;
 using RepRepair.Models.DatabaseModels;
 using RepRepair.Services.AlertService;
+using RepRepair.Services.Cognitive;
 using RepRepair.Services.DB;
 using RepRepair.Services.Language;
 using RepRepair.Services.ScanningService;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace RepRepair.ViewModels
 {
@@ -21,6 +23,7 @@ namespace RepRepair.ViewModels
         private readonly IAlertService _alertService;
         private readonly IDatabaseService _databaseService;
         private readonly LanguageSettingsService _languageSettingsService;
+        private readonly TranslatorService _translatorService;
         private string _reportText;
         public string ReportText
         {
@@ -50,6 +53,7 @@ namespace RepRepair.ViewModels
             _alertService = ServiceHelper.GetService<IAlertService>();
             _databaseService = ServiceHelper.GetService<IDatabaseService>();
             _languageSettingsService = ServiceHelper.GetService<LanguageSettingsService>();
+            _translatorService = ServiceHelper.GetService<TranslatorService>();
             OnSubmit = new Command(async () => await SubmitTextReport());
         }
 
@@ -60,11 +64,14 @@ namespace RepRepair.ViewModels
                 await _alertService.ShowAlertAsync("Alert", "No input", "OK");
                 return;
             }
-            var newReportData = new ReportInfo
+                var textTobeTranslated = ReportText;
+                var translation = await _translatorService.TranslateTextAsync(textTobeTranslated, "sv", SelectedLanguage);
+
+                var newReportData = new ReportInfo
             {
                 SelectedLanguage = _languageSettingsService.CurrentLanguage,
                 OriginalFaultReport = ReportText,
-                TranslatedFaultReport = null,
+                TranslatedFaultReport = translation,
                 TypeOfReport = "Write Message",
                 //QRCode = ObjectInfo.QRCode,
                 ObjectId = ObjectInfo.ObjectId,
